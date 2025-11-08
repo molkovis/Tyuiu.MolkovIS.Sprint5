40b455124cbfa402c4c38152087340bf1159ce46
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-11-08 17:55:10 +0500

    Task3

---
+++ Tyuiu.MolkovIS.Sprint5.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MolkovIS.Sprint5.Task3.V14.Lib
+{
+    public class DataService : ISprint1Task3V14
+    {
+        public double ReverseNumber(double number)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            double result;
+
+            using(BinaryWriter writer = new BinaryWriter(File.Open(path,FileMode.OpenOrCreate)))
+            {
+                result = (4 * Math.Pow(number , 3)) / (Math.Pow(number, 3) - 1);
+                writer.Write(BitConverter.GetBytes(result));
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint5.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MolkovIS.Sprint5.Task3.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

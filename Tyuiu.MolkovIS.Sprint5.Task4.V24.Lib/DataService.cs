1c8f6b88f61ae9fdf9d2eae332d96c1d7eb6f23b
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-11-15 13:27:46 +0500

    Task4

---
+++ Tyuiu.MolkovIS.Sprint5.Task4.V24.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolkovIS.Sprint5.Task4.V24.Lib
+{
+    public class DataService : ISprint5Task4V24
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            double x = Double.Parse(text, NumberStyles.Float, CultureInfo.InvariantCulture);
+
+
+
+            double res = (1.0 / (x * x) + 2.0) * Math.Sin(x);
+
+            return Math.Round(res,3);
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint5.Task4.V24/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.MolkovIS.Sprint5.Task4.V24.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"C:\DataSprint5\InPutDataFileTask4V24.txt";
+
+        DataService dataService = new DataService();
+
+        double res = dataService.LoadFromDataFile(path);
+        Console.WriteLine(res);
+    }
+}

291a9f0d84e6bdf39f421acef010895c7d9b2274
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-11-15 16:02:23 +0500

    Task7

---
+++ Tyuiu.MolkovIS.Sprint5.Task7.V9.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolkovIS.Sprint5.Task7.V9.Lib
+{
+    public class DataService : ISprint5Task7V9
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string outputPath = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V9.txt");
+            string text = File.ReadAllText(path);
+            StringBuilder newText = new StringBuilder();
+            foreach (char c in text)
+            {
+                if (c < 'A' || c > 'Z')
+                {
+                    newText.Append(c);
+                }
+            }
+            File.WriteAllText(outputPath, newText.ToString());
+            return outputPath;
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint5.Task7.V9.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MolkovIS.Sprint5.Task7.V9.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

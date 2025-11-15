e1d155f87d8acd4c6d77a3c6922bd6b030660f16
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-11-15 15:38:15 +0500

    Task6

---
+++ Tyuiu.MolkovIS.Sprint5.Task6.V24.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolkovIS.Sprint5.Task6.V24.Lib
+{
+    public class DataService : ISprint5Task6V24
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string text = File.ReadAllText(path);
+
+            string[] words = text.Split(' ');
+
+            int count = 0;
+
+            foreach (string word in words)
+            {
+                if (word.Length == 5)
+                {
+                    bool isAllDigits = true;
+                    foreach (char c in word)
+                    {
+                        if (!char.IsDigit(c))
+                        {
+                            isAllDigits = false;
+                            break;
+                        }
+                    }
+
+                    if (isAllDigits)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint5.Task6.V24.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MolkovIS.Sprint5.Task6.V24.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

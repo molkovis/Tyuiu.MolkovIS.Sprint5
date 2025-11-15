fc3a00cde1868adb18073d1b099f4ebe6568b33b
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-11-15 14:37:03 +0500

    Task5

---
+++ Tyuiu.MolkovIS.Sprint5.Task5.V16.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MolkovIS.Sprint5.Task5.V16.Lib
+{
+    public class DataService : ISprint5Task5V16
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            string[] strings = text.Split(" ");
+            List<int> integerValues = new List<int>();
+
+            foreach (string s in strings)
+            {
+                if (int.TryParse(s, out int number))
+                {
+                    integerValues.Add(number);
+                }
+            }
+
+            var multiplesOfTen = integerValues.Where(num => num % 10 == 0);
+
+            int maxResult = multiplesOfTen.Max();
+            return maxResult;
+        }
+
+    }
+}
+
---
+++ Tyuiu.MolkovIS.Sprint5.Task5.V16.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MolkovIS.Sprint5.Task5.V16.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
